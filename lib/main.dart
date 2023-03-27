02d0454d6f14d83cb8dafbe7ef3dd1bfd7dc4524
Okorie_Christian <okoriec01@gmail.com>
2023-03-27 07:58:41 +0000

    mondays owrk

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,33 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+// ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: const MyHomePage(
-        title: 'Flutter Title',
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  final String title;
-  const MyHomePage({required this.title});
-
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int counter = 0;
-
-  void incrementCounter() {
-    setState(() {
-      counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        backgroundColor: Colors.lightGreen,
+        appBar: AppBar(
+          backgroundColor: Colors.green,
+          title: const Text("GeeksforGeeks"),
+        ), // AppBar
+        body: Column(
           children: [
-            const Text(
-              'You have clicked the button',
-              style: TextStyle(fontSize: 22),
+            const Center(
+              child: Text("Hello Geeks!!"),
             ),
-            Text(
-              '$counter',
-              style: const TextStyle(fontSize: 40, color: Colors.purpleAccent),
-            ),
-            const Text(
-              'Times',
-              style: TextStyle(fontSize: 22),
-            )
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => incrementCounter(),
-        child: const Icon(Icons.arrow_upward),
-      ),
-    );
+        ), // Container
+      ), // Scaffold
+    ); // MaterialApp
   }
 }
