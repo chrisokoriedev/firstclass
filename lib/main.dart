abe1fbafe67b16f5ddac4ec2150b74ce2e25bbb1
Okorie_Christian <okoriec01@gmail.com>
2023-03-30 08:22:22 +0000

    new login ui

--- lib/main.dart
+++ lib/main.dart
@@ -14,55 +14,10 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color(0xffEDF1D6),
-        appBar: AppBar(
-          backgroundColor: Colors.green,
-          title: const Text("GeeksforGeeks"),
-        ), // AppBar
-        body: Container(
-          width: double.infinity,
-          height: 300,
-          margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 50),
-          padding: const EdgeInsets.all(30),
-          alignment: Alignment.center,
-          transform: Matrix4.rotationZ(0.1),
-          decoration: BoxDecoration(
-            color: const Color(0xff9DC08B),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(20),
-              bottomRight: Radius.circular(20),
-            ),
-            border: Border.all(
-                color: const Color(0xff40513B),
-                width: 3,
-                style: BorderStyle.solid),
-            boxShadow: [
-              const BoxShadow(
-                color: Color(0xff996073),
-                offset: Offset(7, 7),
-                blurRadius: 20,
-                spreadRadius: 10,
-              ),
-            ],
-          ),
-          child: const Text(
-            "Hello! i am inside a container!",
-            style: TextStyle(fontSize: 20),
-          ),
-        ),
-// Radius.circular(20)
-        bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.lightGreen,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.abc), label: 'Home'),
-          ],
-        ), // Container
-      ), // Scaffold
+        body: Container(),
+      ),
     ); // MaterialApp
   }
 }
